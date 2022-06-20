0eb084b236b99bc0ad0e0bc0eb1615d132f94159
nargyanti <nargyanti@gmail.com>
2022-06-20 15:08:44 +0700

    Add criteria's model, seeder, and migration

---
+++ database/seeders/CriteriaSeeder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+use Carbon\Carbon;
+
+class CriteriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $criterias =
+        [
+            [
+                'name' => 'Biologi',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Fisika',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Kimia',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'KMB',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'KPU',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'KUA',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Matematika',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'PPU',
+                'pairwise_score' => 0,
+                'weight' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+        ];
+
+        DB::table('criterias')->insert($criterias);
+    }
+}
